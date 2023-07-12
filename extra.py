9454836e24136a99ee63305ac7edfeb7690ba355
guilhermedelucas <gdelucasde@gmail.com>
2023-07-12 21:10:48 +0200

    Extra - pdf from text

---
+++ extra.py
@@ -0,0 +1,28 @@
+import os.path
+import glob
+from fpdf import FPDF
+from pathlib import Path
+
+TEXT_PATHS = 'texts'
+if not os.path.exists(TEXT_PATHS):
+    os.makedirs(TEXT_PATHS)
+
+LINE_HEIGHT = 10
+
+filepaths = glob.glob('texts/*txt')
+
+for filepath in filepaths:
+    filename = Path(filepath).stem
+    title = filename.title()
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+
+    pdf = FPDF(orientation='P', unit="mm", format="A4")
+
+    pdf.add_page()
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=15, h=LINE_HEIGHT, txt=title, ln=True)
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.multi_cell(w=180, h=6, txt=content)
+    pdf.output(f"{TEXT_PATHS}/{filename}.pdf")
